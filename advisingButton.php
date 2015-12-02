d024a0ece064a8e1c23b23a4390e1338f092eee1
Matt <jmatt3@umbc.edu>
2015-12-02 17:07:52 -0500

    Updated footer to link to advising homepage

---
+++ advisingButton.php
@@ -0,0 +1,16 @@
+<br>
+<br>
+        <img src="./images/paw.png" border="0" height="42" width="42" style="float: right;" onClick="window.open('http://advising.umbc.edu/', '_blank');" ?>
+<br>
+<br>
+
+
+
+
+
+
+
+
+
+
+
